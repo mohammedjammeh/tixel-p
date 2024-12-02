36625a69e07a309e9dfd22c270dd17b737dd6bd8
Mohammed Jammeh <mohammed.jammeh@elasticstage.com>
2024-12-02 23:00:39 +0000

    orders axios call on home

--- Modules/Order/resources/assets/js/components/Home.vue
+++ Modules/Order/resources/assets/js/components/Home.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Home</h1>
+    <h1>Hey</h1>
 </template>
+
+<script setup>
+    import { ref, computed, onMounted } from "vue";
+
+    onMounted(() => {
+        return axios.get('/orders')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error.response.data.errors);
+            });
+    });
+</script>
