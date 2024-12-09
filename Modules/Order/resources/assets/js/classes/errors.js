c5dded337a6383f8c4d695e209d39c088a85d343
Mohammed Jammeh <mohammed.jammeh@elasticstage.com>
2024-12-09 00:34:17 +0000

    Delete unused methods on Order/../error.js

--- Modules/Order/resources/assets/js/classes/errors.js
+++ Modules/Order/resources/assets/js/classes/errors.js
@@ -3,14 +3,6 @@ export default class Errors {
         this.errors = {};
     }
 
-    has(field) {
-        return this.errors.hasOwnProperty(field);
-    }
-
-    any() {
-        return Object.keys(this.errors).length > 0;
-    }
-
     get(field) {
         if (this.errors[field]) {
             return this.errors[field][0];
